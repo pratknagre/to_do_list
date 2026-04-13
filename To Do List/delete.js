d2e3ce334b4823fb6b87f85ef039f1777b8ced78
Pratik Nagre <nagrepratik8135@gmail.com>
2026-04-13 17:28:45 +0530

    Add files via upload

---
+++ To Do List/delete.js
@@ -0,0 +1,42 @@
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value;
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <span onclick="toggleTask(this)">${taskText}</span>
+        <button class="delete" onclick="deleteTask(this)">X</button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
+
+function toggleTask(element) {
+    element.parentElement.classList.toggle("completed");
+}
+
+function deleteTask(button) {
+    const li = button.parentElement;
+    li.classList.add("fade-out");
+
+    setTimeout(() => {
+        li.remove();
+    }, 400);
+}
+
+
+function editTask(button) {
+    const li = button.parentElement.parentElement;
+    const span = li.querySelector("span");
+
+    const currentText = span.innerText;
+
+    const newText = prompt("Edit your task:", currentText);
+
+    if (newText !== null && newText.trim() !== "") {
+        span.innerText = newText;
+    }
+}
